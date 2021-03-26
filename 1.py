be8d584af07e36e67faa6a478934920c0c23af28
Tardis3 <bagrianskii.liu@phystech.edu>
2021-03-26 12:18:19 +0300

    added DecToList, lightNumber, shiftList and runningPattern

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
 leds = [24, 25, 8, 7, 12, 16, 20, 21]
 
 def lightUp(ledNumber, period):
@@ -12,7 +12,7 @@ def lightUp(ledNumber, period):
     GPIO.output(n, 0)
     #GPIO.output(leds, 0)
 
-lightUp(6,0)
+#lightUp(6,0)
 
 
 
@@ -58,12 +58,55 @@ def runningDark(count, period):
 
 #runningDark(2, 1)
 
-def lightNumber(n):
+def decToBinList(n):
     n = bin(n)
-    n = n*2
-    print(n)
-    #for i in range(0, 5):
+    arr = list(n)
+    leng = len(arr)
+    res = [0, 0, 0, 0, 0, 0, 0, 0]
+    leng = leng - 2
+    for i in range(8 - leng, 8):
+        res[i] = arr[i -6 + leng]
+    print(res)
+    return res
+arr = decToBinList(254)
 
-#lightNumber(3)
+
+def shift(lst, steps):
+    if steps < 0:
+        steps = abs(steps)
+        for i in range(steps):
+            lst.append(lst.pop(0))
+    else:
+        for i in range(steps):
+            lst.insert(0, lst.pop())
+shift(arr, 1)
+print(arr)
+
+arr = decToBinList(254)
+def lightNumber(arr):
+    #arr = decToBinList(pattern);
+    for i in range(0, 8):
+        if(arr[i] == 1):
+            GPIO.setup(leds[i], GPIO.OUT)
+            GPIO.output(leds[i], 1)
+    #GPIO.output(n, 0)
+    time.sleep(2)
+    GPIO.output(n, 0)
+    GPIO.output(leds, 0)
+
+
+def runningPattern(pattern, direcrion):
+    i = 0
+    k = 1
+    if(direcrion == "left"):
+        k = -1
+    arr = decToBinList(pattern)
+    while(True):
+        shift(arr, k)
+        lightNumber(arr)
+
+
+#def runningPattern(pattern, direction):
+    
 #GPIO.output(leds, 0)
-GPIO.cleanup()
+#GPIO.cleanup()
